53fbd8c625c27faa83f6a6423353ca8305ca7868
Luis Franco <jose.fnavarro@alumnos.udg.mx>
2020-09-29 13:16:06 -0500

    programa para calcular el valor de e

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var e float64
+	var exponencial, limite int
+	e = 1
+
+	fmt.Scanln(&limite)
+
+	for i := 1; i <= limite; i++ {
+		exponencial = 1
+		for j := 1; j <= i; j++ {
+			exponencial = exponencial * j	
+		}
+		e = e + 1/float64(exponencial)
+	}
+	fmt.Println(e)
 }
